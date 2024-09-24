512eff6c83b719f84923db2d757f194b4798fac3
StEgo2103 <luca@deltort.net>
2024-09-23 17:50:12 -0700

    Add style and squelette

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,43 @@
-<h1>Portfolio by Luca Deltort</h1>
+<div class="mx-10">
+    <div class="fixed right-10">
+        <div class="flex justify-end">
+            <div class="flex gap-x-5 bg-opacity-50 bg-mySurface drop-shadow-md p-3 mt-5 rounded-xl">
+                <a class="navbar" href="#home">Home</a>
+                <a href="#competencies">Competencies</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+                <div class="border-l-1 border-black h-auto"></div>
+            </div>
+        </div>
+    </div>
+	<div class="flex items-center h-screen">
+		<div class="w-auto">
+			<h1 class="text-8xl pb-2 font-normam">
+				<i>L</i>uca Deltort
+			</h1>
+			<div class="border-t-1 border-black"></div>
+            <h2 class="text-5xl pt-2 font-light">
+                Web & Mobile Developer
+            </h2>
+		</div>
+	</div>
+    <div class="flex items-center h-screen">
+		Competencies
+	</div>
+    <div class="flex items-center h-screen">
+		Projects
+	</div>
+    <div class="flex items-center h-screen">
+		Contact
+	</div>
+</div>
+
+<style lang="postcss">
+	.border-l-1 {
+		border-left-width: 1px;
+	}
+
+	.border-t-1 {
+		border-top-width: 1px;
+	}
+</style>
